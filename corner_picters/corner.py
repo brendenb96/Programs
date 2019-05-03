dcf7b31daebec69a3b0217d26f61d6235f946025
brenden <brenden.j.bickner@gmail.com>
2019-05-02 22:07:12 -0600

    added corner check

---
+++ corner_picters/corner.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import sys
+from PIL import Image
+
+SAMPLE_SIZE = 300
+SAMPLE_RANGE = range(0,SAMPLE_SIZE)
+REPLACE_PIXEL = (255,0,0)
+
+def main():
+
+    imagePath = "picture.jpeg"
+    newImagePath = "picture2.jpeg"
+    im = Image.open(imagePath)
+    pix = im.load()
+
+    sample_count = (SAMPLE_SIZE * SAMPLE_SIZE) / 2
+
+    photo_y_size, photo_x_size = im.size
+    photo_x_size -= 1
+    photo_y_size -= 1
+
+    print "X Size: " + str(photo_x_size)
+    print "Y Size: " + str(photo_y_size)
+
+    # NW
+    nw_sum = 0
+    column_count = SAMPLE_SIZE
+    for row in SAMPLE_RANGE:
+        print_out = ""
+        column_sample = range(0,column_count)
+        for column in column_sample:
+            pixel = pix[row, column]
+            im.putpixel((row, column), REPLACE_PIXEL)
+            nw_sum += sum(pixel)
+        column_count -= 1
+
+    nw_avg = nw_sum / sample_count
+    print "NW AVG: %s" % nw_avg
+
+    # ne
+    ne_sum = 0
+    column_count = SAMPLE_SIZE
+    for row in SAMPLE_RANGE:
+        print_out = ""
+        column_sample = range(0,column_count)
+        for column in column_sample:
+            pixel = pix[row, (photo_x_size - column)]
+            im.putpixel((row, (photo_x_size - column)), REPLACE_PIXEL)
+            ne_sum += sum(pixel)
+        column_count -= 1
+
+    ne_avg = ne_sum / sample_count
+    print "NE AVG: %s" % ne_avg
+
+    # sw
+    sw_sum = 0
+    column_count = SAMPLE_SIZE
+    for row in SAMPLE_RANGE:
+        print_out = ""
+        column_sample = range(0,column_count)
+        for column in column_sample:
+            pixel = pix[(photo_y_size - row), column]
+            im.putpixel(((photo_y_size - row), column), REPLACE_PIXEL)
+            sw_sum += sum(pixel)
+        column_count -= 1
+
+    sw_avg = sw_sum / sample_count
+    print "SW AVG: %s" % sw_avg
+
+    # se
+    se_sum = 0
+    column_count = SAMPLE_SIZE
+    for row in SAMPLE_RANGE:
+        print_out = ""
+        column_sample = range(0,column_count)
+        for column in column_sample:
+            pixel = pix[(photo_y_size - row), (photo_x_size - column)]
+            im.putpixel(((photo_y_size - row), (photo_x_size - column)), REPLACE_PIXEL)
+            se_sum += sum(pixel)
+        column_count -= 1
+
+    se_avg = se_sum / sample_count
+    print "SE AVG: %s" % se_avg
+
+    im.save(newImagePath)
+
+    return 0
+
+# Entry Point
+#############################
+if __name__ == "__main__":
+    sys.exit(main())
+#############################
